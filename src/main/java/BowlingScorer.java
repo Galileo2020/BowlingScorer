f0ad3b88ff29d2bed600ab2f2c71e7ff3ec20072
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-06-04 13:53:41 +0800

    [xiaojun] fast fix test

---
+++ src/main/java/BowlingScorer.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class BowlingScorer {
+
+    private List<Round> rounds;
+
+    public BowlingScorer(List<Round> rounds) {
+
+        this.rounds = rounds;
+    }
+
+    public int calculateTotalScores() {
+        return 0;
+    }
+}
---
+++ src/main/java/Round.java
@@ -0,0 +1,10 @@
+public class Round {
+
+    private String firstHit;
+    private String secondHit;
+
+    public Round(String firstHit, String secondHit) {
+        this.firstHit = firstHit;
+        this.secondHit = secondHit;
+    }
+}
