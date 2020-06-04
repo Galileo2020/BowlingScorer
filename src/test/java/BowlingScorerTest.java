09a5530c01ac5381fe0a4003ab3b495f8405e90a
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-06-04 15:19:07 +0800

    [xiaojun] add test `shouldReturnTheTotalAmountOfEveryRoundsAndTheNextTwoHitAfterStrikeWhenCalculateTotalScoresGivenTheLastRoundHitStrikeAndEveryOtherRoundHaveNotHitAllTheBalls`

--- src/test/java/BowlingScorerTest.java
+++ src/test/java/BowlingScorerTest.java
@@ -103,4 +103,25 @@ public class BowlingScorerTest {
         // Then
         Assertions.assertEquals(0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + (10 + 1  + 8) + 9, totalScores);
     }
+
+    @Test
+    void shouldReturnTheTotalAmountOfEveryRoundsAndTheNextTwoHitAfterStrikeWhenCalculateTotalScoresGivenTheLastRoundHitStrikeAndEveryOtherRoundHaveNotHitAllTheBalls () {
+        // Given
+        Round round0 = new Round("0", "0");
+        Round round1 = new Round("0", "1");
+        Round round2 = new Round("1", "1");
+        Round round3 = new Round("1", "2");
+        Round round4 = new Round("1", "3");
+        Round round5 = new Round("1", "4");
+        Round round6 = new Round("1", "5");
+        Round round7 = new Round("1", "6");
+        Round round8 = new Round("1", "7");
+        Round round9 = new Round("X");
+        Round round10 = new Round("1", "2");
+        BowlingScorer bowlingScorer = new BowlingScorer(Arrays.asList(round0, round1, round2, round3, round4, round5, round6, round7, round8, round9, round10));
+        // When
+        int totalScores = bowlingScorer.calculateTotalScores();
+        // Then
+        Assertions.assertEquals(0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + (10 + 1 + 2), totalScores);
+    }
 }
